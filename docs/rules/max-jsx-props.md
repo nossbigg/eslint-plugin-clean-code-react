b8131f547fcf6fd7ffd0fa7431e5325f8e9a2fce
nossbigg <chenggibson@gmail.com>
2022-03-02 22:21:54 +0800

    [max-jsx-props] Add initial implementation

---
+++ docs/rules/max-jsx-props.md
@@ -0,0 +1,46 @@
+# max-jsx-props
+
+Ensures that `JSXElement` does not exceed max number of props.
+
+## Rule Details
+
+`JSXElement` with many props is an indication that the component is doing too much.
+
+Examples of **incorrect** code for this rule:
+
+```js
+// >5 props
+const MyComponent: React.FC = () => (
+  <AnotherComponent
+    firstName={firstName}
+    lastName={lastName}
+    user={user}
+    isAdmin
+    isViewOnly
+    {...otherExtraProps}
+  />
+);
+```
+
+Examples of **correct** code for this rule:
+
+```js
+// 5 props or less
+const MyComponent: React.FC = () => (
+  <AnotherComponent
+    firstName={firstName}
+    lastName={lastName}
+    user={user}
+    isAdmin
+    isViewOnly
+  />
+);
+```
+
+### Options
+
+- `maxProps`: `number`, default: `5`
+
+## Further Reading
+
+- [TypeScript: Documentation - JSX](https://www.typescriptlang.org/docs/handbook/jsx.html)
---
+++ tests/lib/rules/max-jsx-props.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview tbc
+ * @author nossbigg
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/max-jsx-props"),
+  RuleTester = require("eslint").RuleTester;
+const { getRuleTesterConfig } = require("../get-rule-tester-config");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester(getRuleTesterConfig());
+ruleTester.run("max-jsx-props", rule, {
+  valid: [
+    `
+    const MyComponent: React.FC = () => <AnotherComponent
+      firstName={firstName}
+      lastName={lastName}
+      user={user}
+      isAdmin
+      isShowOnly
+    />
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+      const MyComponent: React.FC = () => <AnotherComponent
+      firstName={firstName}
+      lastName={lastName}
+      user={user}
+      isAdmin
+      isShowOnly
+      {...someOtherProps}
+    />`,
+      errors: [
+        {
+          message: "JSX Element exceeds max props.",
+          type: "JSXOpeningElement",
+        },
+      ],
+    },
+  ],
+});
