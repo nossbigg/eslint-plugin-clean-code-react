917eb7616134b97f2f1997f5e2030dd2abe25626
nossbigg <chenggibson@gmail.com>
2022-03-11 01:31:57 +0800

    [max-jsx-prop-value-lines] Add initial implementation

---
+++ docs/rules/max-jsx-prop-value-lines.md
@@ -0,0 +1,37 @@
+# max-jsx-prop-value-lines
+
+Ensures that JSX prop value does not exceed max lines.
+
+## Rule Details
+
+Verbose JSX prop values add unnecessary complexity to a JSX element definition. For long prop values (eg. expressions), it is preferable to refactor the long prop value into a separate variable.
+
+This rule aims to...
+
+Examples of **incorrect** code for this rule:
+
+```js
+// not ok, jsx prop value exceeds 1 line
+<Row
+  onClick={(value) => {
+    const normalizedValue = trim(value);
+    updateRow(normalizedValue);
+  }}
+/>
+```
+
+Examples of **correct** code for this rule:
+
+```js
+// ok
+<Row onClick={onRowClick} />;
+
+const onRowClick = (value) => {
+  const normalizedValue = trim(value);
+  updateRow(normalizedValue);
+};
+```
+
+### Options
+
+- `maxPropLines`: `number`, default: `1`
---
+++ tests/lib/rules/max-jsx-prop-value-lines.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview tbc
+ * @author nossbigg
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/max-jsx-prop-value-lines"),
+  RuleTester = require("eslint").RuleTester;
+const { getRuleTesterConfig } = require("../get-rule-tester-config");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester(getRuleTesterConfig());
+const ruleOptions = [{ maxPropLines: 1 }];
+ruleTester.run("max-jsx-prop-value-lines", rule, {
+  valid: [
+    {
+      code: `const MyComponent: React.FC = () => { 
+        return <Row 
+          onClick={() => { console.log('clicked!'); }}
+        />
+      };
+  `,
+      options: ruleOptions,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `const MyComponent: React.FC = () => { 
+        return <Row 
+          onClick={() => {
+            console.log('clicked!');
+          }}
+        />
+      };`,
+      errors: [
+        {
+          message: "JSX prop value exceeds max lines.",
+          type: "JSXAttribute",
+        },
+      ],
+      options: ruleOptions,
+    },
+  ],
+});
