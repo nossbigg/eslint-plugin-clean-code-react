76908e7944573b7c7155ce0a11fbc121f77bc69e
nossbigg <chenggibson@gmail.com>
2022-03-11 02:55:37 +0800

    [max-component-nested-fns] Add initial implementation

---
+++ docs/rules/max-component-nested-fns.md
@@ -0,0 +1,72 @@
+# max-component-nested-fns
+
+Ensures component does not exceed max number of nested functions.
+
+## Rule Details
+
+Components with too many nested functions are hard to test and difficult to reason.
+
+Examples of **incorrect** code for this rule:
+
+```js
+// function component, >5 nested functions
+const MyFnComponent: React.FC = () => {
+  const onClick1 = () => {};
+  const onClick2 = function () {};
+  function onClick3() {}
+
+  const onClick5 = () => {
+    const onClick6 = () => {};
+  };
+
+  return <div />;
+};
+
+// class component, >5 nested functions
+// excludes render() and constructor() definition by default
+class MyClassComponent extends React.Component {
+  constructor() {}
+  render() {
+    return <div />;
+  }
+
+  onClick1() {}
+  onClick2 = () => {};
+  onClick3() {}
+
+  onClick5() {
+    const onClick6 = () => {};
+  }
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+// function component, 5 or less nested functions
+const MyFnComponent: React.FC = () => {
+  const onClick1 = () => {};
+  const onClick2 = function () {};
+  function onClick3() {}
+
+  return <div />;
+};
+
+// class component, 5 or less nested functions
+// excludes render() and constructor() definition by default
+class MyClassComponent extends React.Component {
+  constructor() {}
+  render() {
+    return <div />;
+  }
+
+  onClick1() {}
+  onClick2 = () => {};
+  onClick3() {}
+}
+```
+
+### Options
+
+- `maxFns`: `number`, default: `10`
+- `excludedClassMethods`: `number`, default: `['render', 'constructor']`
---
+++ tests/lib/rules/max-component-nested-fns.js
@@ -0,0 +1,96 @@
+/**
+ * @fileoverview tbc
+ * @author nossbigg
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/max-component-nested-fns"),
+  RuleTester = require("eslint").RuleTester;
+const { getRuleTesterConfig } = require("../get-rule-tester-config");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleOptions = [{ maxFns: 3 }];
+const ruleTester = new RuleTester(getRuleTesterConfig());
+ruleTester.run("max-component-nested-fns", rule, {
+  valid: [
+    {
+      code: `const MyComponent: React.FunctionComponent = () => {
+                const fn1 = () => {};
+                const fn2 = function(){};
+                function fn3(){};
+               return <div />
+              }`,
+      options: ruleOptions,
+    },
+    {
+      code: `class MyComponent extends React.Component {
+                constructor(){}
+                render(){}
+                onClick1 = () => {}
+                onClick2(){
+                  const onClick3 = () => {}
+                }
+              }`,
+      options: ruleOptions,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `const MyComponent: React.FunctionComponent = () => {
+        const fn1 = () => {};
+        const fn2 = function(){};
+        function fn3(){};
+        const fn4 = () => {};
+       return <div />
+      }`,
+      errors: [
+        {
+          message: "React component exceeds max number of nested functions.",
+          type: "VariableDeclaration",
+        },
+      ],
+      options: ruleOptions,
+    },
+    {
+      code: `class MyComponent extends React.Component {
+        constructor(){}
+        render(){}
+        onClick1 = () => {}
+        onClick2(){
+          function onClick3(){}
+        }
+        onClick4(){}
+      }`,
+      errors: [
+        {
+          message: "React component exceeds max number of nested functions.",
+          type: "ClassDeclaration",
+        },
+      ],
+      options: ruleOptions,
+    },
+    {
+      code: `class MyComponent extends React.Component {
+        constructor(){}
+        render(){}
+        onClick1 = () => {}
+        onClick2(){}
+      }`,
+      errors: [
+        {
+          message: "React component exceeds max number of nested functions.",
+          type: "ClassDeclaration",
+        },
+      ],
+      options: [{ maxFns: 3, excludedClassMethods: [] }],
+    },
+  ],
+});
